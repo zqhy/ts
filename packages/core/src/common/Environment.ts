8b490e2103ec36f6adc19e0c5035be22aa550aee
innwin <innwin@live.com>
2023-02-20 08:37:42 +0800

    `Environment` 删除 `IsDark`

--- packages/core/src/common/Environment.ts
+++ packages/core/src/common/Environment.ts
@@ -1,18 +1,3 @@
 export class Environment {
     public static readonly isServerSide = typeof window === 'undefined';
-
-    public static IsDark(localStorageKey: string | null = "theme"): boolean | undefined {
-        if (Environment.isServerSide) {
-            return undefined;
-        }
-
-        if (localStorageKey) {
-            const theme = localStorage.getItem(localStorageKey);
-            if (theme) {
-                return theme === "dark";
-            }
-        }
-
-        return window.matchMedia("(prefers-color-scheme: dark)").matches
-    }
 }
