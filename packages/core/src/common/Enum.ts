9aca81b63b1c0177631cfc0b300223ddf6454b60
DongHuaShao <708990353@qq.com>
2024-01-10 16:35:02 +0800

    增加 `Enum.get`

--- packages/core/src/common/Enum.ts
+++ packages/core/src/common/Enum.ts
@@ -1,21 +1,37 @@
 type EnumValueType = string | number | symbol;
 type EnumType = { [key in EnumValueType]: EnumValueType };
 type ValueOf<T> = T[keyof T];
-type EnumItems<T> = Array<ValueOf<T>>;
+
+export type EnumObject<T> = {
+  name: string;
+  value: ValueOf<T>;
+};
 
 export class Enum {
-  static getValues<T>(enumCLass: EnumType & T): EnumItems<T> {
+  static getValues<T>(enumType: EnumType & T): Array<ValueOf<T>> {
     return (
       // get enum keys and values
-      Object.values(enumCLass)
+      Object.values(enumType)
         // Half of enum items are keys and half are values so we need to filter by index
         .filter((e, index, array) => index < array.length / 2)
         // finally map items to enum
-        .map((e) => enumCLass[e as keyof T]) as EnumItems<T>
+        .map((e) => enumType[e as keyof T]) as Array<ValueOf<T>>
     );
   }
 
-  static getNames<T>(enumCLass: EnumType & T): string[] {
-    return Object.keys(enumCLass).filter((key) => Number.isNaN(Number(key)));
+  static getNames<T>(enumType: EnumType & T): string[] {
+    return Object.keys(enumType).filter((key) => Number.isNaN(Number(key)));
+  }
+
+  static get<T>(enumType: EnumType & T): Array<EnumObject<T>> {
+    const values = Object.values(enumType);
+    const enumObjects = new Array<EnumObject<T>>();
+    for (let i = 0; i < values.length / 2; i++) {
+      enumObjects.push({
+        name: values[i] as string,
+        value: enumType[values[i] as keyof T] as ValueOf<T>,
+      });
+    }
+    return enumObjects;
   }
 }
--- packages/core/test/common/testEnum.ts
+++ packages/core/test/common/testEnum.ts
@@ -5,6 +5,8 @@ enum TestEnum {
   b,
   c,
 }
+type TestEnumType = keyof typeof TestEnum;
+
 test('Enum.getValues', () => {
   const tests = Enum.getValues(TestEnum);
   expect(TestEnum.a).toBe(0);
@@ -16,7 +18,22 @@ test('Enum.getNames', () => {
   const tests = Enum.getNames(TestEnum);
   expect(tests.length).toBe(3);
   expect(tests[1] === 'b').toBe(true);
+  // for (const test of tests) {
+  //   console.log(`name : ${test}`);
+  // }
+  //
+  // const test = TestEnum['z' as TestEnumType];
+  // console.log(test ?? TestEnum.a);
+});
+
+test('Enum.get', () => {
+  const values = Object.values(TestEnum);
+  console.log(`所有值：${values}`);
+
+  const tests = Enum.get(TestEnum);
   for (const test of tests) {
-    console.log(`name : ${test}`);
+    console.log(`name : ${test.name}, value: ${test.value}`);
   }
+  // expect(TestEnum.a).toBe(0);
+  // expect(tests.length).toBe(3);
 });
